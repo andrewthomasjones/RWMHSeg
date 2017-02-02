1658571577d0e90cc10c32d9fd87164c029a5c44
andrewthomasjones <andrewthomasjones@gmail.com>
2017-02-02 17:10:06 +1000

    Update howTo.sh

--- howTo.sh
+++ howTo.sh
@@ -14,14 +14,13 @@
 # .../out_mxxxxxx/WM_modstrip_mxxxxxx.nii, .../out_mxxxxxx/GMCSF_strip_mxxxxxx.nii, .../out_mxxxxxx/Vent_bin_mxxxxxx.nii  
 
 SAMPLE="mxxxxxx" #sample name 
-RFMOD="/folder/xxxx/envisionRFModel_3107_4242.xml" #presaved RF model file
-PMAPCUT="0.5"  #cut-off for pmap for calulating volume 
-P_THRESH="0.02"  #cut-off for t-dist model
+RFMOD=".../envisionRFModel_3107_4242.xml" #presaved RF model file
+P_THRESH="0.02"  #cut-off for t-dist model 
 D_THRESH="1.35"  #cut-off for m-estimator robust normal model
 MIN_N="3"  #min. neighbours for m-estimator robust normal model
 
-OUTFOLD="/folder/xxxx/Output/" #output base folde, will save like ~/Output/mxxxxxx/. code does NOT create new folders and does not WARN. To be fixed.
-BASEFOLD="/folder/xxxx/out_"  #input base folder i.e. "~/TrainingData/CAI_itk_w2mhs/itk_data/out_mxxxxxx/"
+OUTFOLD=".../Output/" #output base folde, will save like ~/Output/mxxxxxx/. code does NOT create new folders and does not WARN. To be fixed.
+BASEFOLD=".../out_"  #input base folder i.e. "~/TrainingData/CAI_itk_w2mhs/itk_data/out_mxxxxxx/"
 
 SAMPLEFOLD=$BASEFOLD$SAMPLE"/"
 MYFOLD=$OUTFOLD$SAMPLE"/"
